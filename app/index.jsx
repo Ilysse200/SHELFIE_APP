69f8acf73788781a53cb1a45de2ff2e631efa68f
Ilysse200 <ilysseted500@gmail.com>
2025-06-04 15:11:19 +0200

    removed the default root component and entry file

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+//Represent the home page component for the application
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
