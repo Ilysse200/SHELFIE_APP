18f10688fd770a238bf9394d9d562a4614de56bb
Ilysse200 <ilysseted500@gmail.com>
2025-06-05 11:34:40 +0200

    added custom texts and styled them

--- app/index.jsx
+++ app/index.jsx
@@ -4,12 +4,22 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      {/**To output text, we use text component in react native */}
+      <Text>The Number 1</Text> 
+      <Text>Reading List App</Text>
+      {/** View component can be thought of as a  div tag in normal react. Its used to wrap
+       *nested elements and for layout and structure*/}
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  }
+})
