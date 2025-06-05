1fb1fe446bd30dab4505b6831167ae053a4381d6
Ilysse200 <ilysseted500@gmail.com>
2025-06-05 12:10:11 +0200

    added an image

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,22 @@
 //Represent the home page component for the application
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Logo from '../assets/images/logo_light.png'
 import React from 'react'
 
 const Home = () => {
   return (
     <View style={styles.container}>
+      {/**Importing the image directly stored locally */}
+      <Image source = {Logo} style={styles.img}/>
       {/**To output text, we use text component in react native */}
-      <Text>The Number 1</Text> 
-      <Text>Reading List App</Text>
+      <Text style={styles.title}>The Number 1</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>
+        Reading List App
+      </Text>
+      {/** */}
+      {/* <View style={styles.card}>
+        <Text>Hello, This is a card</Text>
+      </View> */}
       {/** View component can be thought of as a  div tag in normal react. Its used to wrap
        *nested elements and for layout and structure*/}
     </View>
@@ -17,9 +26,22 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center'
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 22
+  },
+  // card:{
+  //   backgroundColor:'#eee',
+  //   padding:20,
+  //   borderRadius:5,
+  //   boxShadow:'4px 4px rgba(0,0,0,0.1)'
+  // },
+  img:{
+    marginVertical:20
   }
 })
